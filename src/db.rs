9ff70807214400376fb5fe3b9bc07cf0d04ff2e8
steph-crown <emmanuelstephen024@gmail.com>
2026-03-21 13:53:59 +0100

    Add PostgreSQL service configuration in docker-compose.yml, implement database connection pool in db.rs, and update main.rs to establish database connection on startup

---
+++ src/db.rs
@@ -0,0 +1,10 @@
+use crate::config::Config;
+use sqlx::postgres::PgPoolOptions;
+
+pub async fn create_pool(config: &Config) -> Result<sqlx::PgPool, sqlx::Error> {
+  PgPoolOptions::new()
+    .max_connections(10)
+    .acquire_timeout(std::time::Duration::from_secs(3))
+    .connect(&config.database_url)
+    .await
+}
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use config::Config;
 pub mod config;
 pub mod errors;
 
-fn main() -> anyhow::Result<()> {
+mod db;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
   let _ = dotenvy::dotenv();
   let config = Config::load()?;
+  let db_pool = db::create_pool(&config)
+    .await
+    .map_err(|e| anyhow::anyhow!("Could not connect to database. Failed with error: {e}"))?;
 
   Ok(())
 }
