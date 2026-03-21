2bc14f89f3f61e2e8de0c75c5531be5394462905
steph-crown <emmanuelstephen024@gmail.com>
2026-03-21 15:57:31 +0100

    fix

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,1 +1,11 @@
-pub struct User {}
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct User {
+  pub id: Uuid,
+  pub display_name: String,
+  pub created_at: OffsetDateTime,
+}
